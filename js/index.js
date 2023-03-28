846505212e7f38030839007d48540e3274befc11
x-ant-ie <muchadashantel02@gmail.com>
2023-03-28 09:34:31 +0200

    my portfolio

---
+++ js/index.js
@@ -0,0 +1,47 @@
+
+
+function openTab(evt, tabName) {
+    // Declaring variables
+    var i, tabcontent, tablinks;
+  
+    // all elements with class="tabcontent" , hide
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // all elements with class="tablinks" , remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+
+
+  // Get the element with id="defaultOpen" and click on it
+ document.getElementById("defaultOpen").click();
+    
+
+
+ $(document).ready(function(){
+
+ $('.tablinks').click(function(){
+   $('.tablinks').removeClass('').addClass('active');
+
+
+
+  //  $('.tablinks img:first').css({
+  //    'width':'500px',
+  //    'height':'300px'
+  //  });
+ });
+
+
+
+
+
+ });
